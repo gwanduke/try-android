a2e9cb901272e1e8ac19751c5994385db55b76b1
gwanduke <gwanduke@gmail.com>
2021-12-11 02:21:23 +0900

    TodoList 프로젝트 완성

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
 # Try Android
 
-- [Build A Simple Android App With Kotlin](https://www.youtube.com/watch?v=BBWyXo-3JGQ)
-
 ## Gradle
 
 `gradle`은 빌드 엔진이다.
+
+## Android Studio 세팅
+
+### format on save
+
+- edit -> Macros -> Start Macro Recording
+- <kbd>shift</kbd> + <kbd>option</kbd> + <kbd>cmd</kbd> + <kbd>L</kbd> 으로 설정 확인
+- 레코딩 시작
+  - <kbd>option</kbd> + <kbd>cmd</kbd> + <kbd>L</kbd>
+  - <kbd>cmd</kbd> + <kbd>S</kbd>
+- edit -> Macros -> Stop Macro Recording ("format on save")
+- preferences -> keymap -> "format on save" 를 <kbd>cmd</kbd> + <kbd>S</kbd> 로 지정
+
+## Trouble Shooting
+
+### xml에 정의된 id를 import할 수 없음
+
+Android Studio 4.1 부터 Kotlin 코드로 ID를 바로 참조하는 기능이 기본적으로 disabled 되었으므로
+
+=> gradle app plugins에 `id 'kotlin-android-extensions'` 추가
+
+## Projects
+
+### [TodoList](./TodoList)
+
+![TodoList 화면](./docs/images/TodoList.jpg)
--- TodoList/app/src/main/java/com/gwanduke/helloworld/MainActivity.kt
+++ TodoList/app/src/main/java/com/gwanduke/helloworld/MainActivity.kt
@@ -2,11 +2,31 @@ package com.gwanduke.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 
 // 앱의 한 스크린을 Activity 라 한다.
 class MainActivity : AppCompatActivity() {
+    private lateinit var todoAdapter: TodoAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        todoAdapter = TodoAdapter(mutableListOf())
+
+        rvTodoItems.adapter = todoAdapter
+        rvTodoItems.layoutManager = LinearLayoutManager(this)
+
+        btnAddTodo.setOnClickListener {
+            val todoTitle = etTodoTitle.text.toString()
+            if (todoTitle.isNotEmpty()) {
+                val todo = Todo(todoTitle)
+                todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
+            }
+        }
+        btnDeleteTodo.setOnClickListener {
+            todoAdapter.deleteDoneTodos()
+        }
     }
 }
