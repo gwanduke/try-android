47a3f10434aea4bf819e0b4dedc9a34678adfb8e
gwanduke <gwanduke@gmail.com>
2021-12-11 05:07:56 +0900

    DOBCalc 프로젝트 완성
    분단위 나이 계산기

--- DOBCalc/app/src/main/java/com/gwanduke/dobcalc/MainActivity.kt
+++ DOBCalc/app/src/main/java/com/gwanduke/dobcalc/MainActivity.kt
@@ -1,13 +1,69 @@
 package com.gwanduke.dobcalc
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private var tvSelectedDate: TextView? = null;
+    private var tvAgeInMinutes: TextView? = null;
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val buttonDatePicker: Button = findViewById(R.id.buttonDatePicker)
+        tvSelectedDate = findViewById(R.id.tvSelectedDate)
+        tvAgeInMinutes = findViewById(R.id.tvAgeInMinutes)
 
+        buttonDatePicker.setOnClickListener {
+            clickDatePicker(it, 2021, 1)
+        }
+
+
+    }
+
+    private fun clickDatePicker(view: View, year: Int, month: Int) {
+        val myCalendar = Calendar.getInstance()
+        val year = myCalendar.get(Calendar.YEAR)
+        val month = myCalendar.get(Calendar.MONTH)
+        val day = myCalendar.get(Calendar.DAY_OF_MONTH)
+
+        val dpd = DatePickerDialog(
+            this,
+            DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+                Toast.makeText(this, "Datepicker works", Toast.LENGTH_LONG).show()
+
+                val selectedDate = "$year-${month + 1}-$dayOfMonth"
+
+                tvSelectedDate?.text = selectedDate
+
+                val sdf = SimpleDateFormat("yyyy-MM-dd", Locale.KOREAN)
+                val theDate = sdf.parse(selectedDate)
+                theDate?.let {
+                    val selectedDateInMinutes = theDate.time / 60_000
+                    val currentDate = sdf.parse(sdf.format(System.currentTimeMillis()))
+                    currentDate?.let {
+                        val currentDateInMinutes = currentDate.time / 60_000
+                        val result = currentDateInMinutes - selectedDateInMinutes
+
+                        tvAgeInMinutes?.text = result.toString()
+                    }
+                }
+            },
+            year,
+            month,
+            day
+        )
+
+        dpd.datePicker.maxDate = System.currentTimeMillis() - 86_400_000
+        dpd.show();
+
+        Toast.makeText(this, "buttonDatePicker Clicked", Toast.LENGTH_LONG).show();
     }
 }
